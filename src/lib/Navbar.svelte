0ed11d922d53426f726a1ae27dc75fe6b6341412
BenCDA <cardosobenjamin01@gmail.com>
2023-09-13 10:46:43 +0200

    style(navbar):add logo and responsive

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,8 +1,15 @@
 <script></script>
 <nav>
-<img src="/assets/LogoGood.png" alt="Renconsport Logo">
-<img src="/assets/logoapple.png" alt="Logo Apple store">
-<img src="/assets/logogoogle.png" alt="Logo Google Play Store">
+    
+        <div class="left-logo">
+            <img src="/assets/LogoGood.png" alt="Renconsport Logo">
+        </div>
+        <div class="right-logos">
+            <img src="/assets/logoapple.png" alt="Logo Apple store">
+            <img src="/assets/logogoogle.png" alt="Logo Google Play Store">
+        </div>
+    
+    
 </nav>
 
 
@@ -10,10 +17,35 @@
 nav {
     background-color: #7FAAC8;
     display: flex;
-    justify-content: center;
+    justify-content: space-between; /* Pour séparer les logos à gauche et à droite */
+    align-items: center; /* Pour centrer verticalement les logos */
     height: 150px;
-    width: 100%;
+    padding: 0 20px; /* Espace autour des logos pour les séparer du bord de la navbar */
 }
+
+.right-logo {
+    order: 1; 
+}
+
+.left-logos {
+    display: flex;
+    gap: 10px; 
+}
+
+img {
+    height: 100%; 
+}
+
+@media screen and (max-width: 600px) {
+    nav {
+        height: 100px;
+    }
+    
+    img {
+        height: 80px; /* Réduit la taille des logos pour les écrans plus petits */
+    }
+}
+
 
 @media screen and (max-width: 600px) {
     nav {
